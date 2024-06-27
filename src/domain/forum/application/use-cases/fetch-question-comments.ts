79ed3cc0d2822e9a9d87ee286cab43a33e4cc3d4
Flávio Dutra <flaviomdutra@outlook.com>
2024-06-27 16:39:23 -0300

    feat: comments with author

--- src/domain/forum/application/use-cases/fetch-question-comments.ts
+++ src/domain/forum/application/use-cases/fetch-question-comments.ts
@@ -1,7 +1,7 @@
-import { Either, right } from '@/core/either'
 import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
-import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
+import { Either, right } from '@/core/either'
 import { Injectable } from '@nestjs/common'
+import { CommentWithAuthor } from '../../enterprise/entities/value-objects/comment-with-author'
 
 interface FetchQuestionCommentsUseCaseRequest {
   questionId: string
@@ -10,7 +10,9 @@ interface FetchQuestionCommentsUseCaseRequest {
 
 type FetchQuestionCommentsUseCaseResponse = Either<
   null,
-  { questionComments: QuestionComment[] }
+  {
+    comments: CommentWithAuthor[]
+  }
 >
 
 @Injectable()
@@ -21,11 +23,16 @@ export class FetchQuestionCommentsUseCase {
     questionId,
     page,
   }: FetchQuestionCommentsUseCaseRequest): Promise<FetchQuestionCommentsUseCaseResponse> {
-    const questionComments =
-      await this.questionCommentsRepository.findManyByQuestionId(questionId, {
-        page,
-      })
+    const comments =
+      await this.questionCommentsRepository.findManyByQuestionIdWithAuthor(
+        questionId,
+        {
+          page,
+        },
+      )
 
-    return right({ questionComments })
+    return right({
+      comments,
+    })
   }
 }
