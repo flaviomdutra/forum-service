4ffa57ff9d2b8a35fe041ab2b171736a587c8af0
Flávio Dutra <flaviomdutra@outlook.com>
2024-10-28 17:50:49 -0300

    feat: setup swagger

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 import { EnvService } from './env/env.service'
 
@@ -6,6 +7,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     // logger: false,
   })
+
+  const config = new DocumentBuilder()
+    .setTitle('Forum API')
+    .setDescription('Forum API description')
+    .setVersion('1.0')
+    .build()
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, documentFactory)
+
   const configService = app.get(EnvService)
   const port = configService.get('PORT')
 
